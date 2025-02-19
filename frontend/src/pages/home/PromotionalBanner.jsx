920c110f4e4bcc170d5663e7076b7a7db2257180
Malaika Ali <114911365+Malaika-Ali@users.noreply.github.com>
2025-02-20 00:50:26 +0500

    Update PromotionalBanner.jsx

--- frontend/src/pages/home/PromotionalBanner.jsx
+++ frontend/src/pages/home/PromotionalBanner.jsx
@@ -1,4 +1,4 @@
-import img from '../../assets/img.JPG'
+import img from '../../assets/img.jpg'
 import { LazyLoadImage } from 'react-lazy-load-image-component';
 import 'react-lazy-load-image-component/src/effects/blur.css';
 
