b7bd0d402cb106c667e60d67d222e5a445f41978
Malaika Ali <114911365+Malaika-Ali@users.noreply.github.com>
2025-02-20 11:59:17 +0500

    Update index.js to remove .env file path

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,10 @@
 import {connectDB} from '../src/database/index.js'
-import dotenv from 'dotenv'
+// import dotenv from 'dotenv'
 import {app} from "./app.js"
 
-dotenv.config({
-    path: './.env'
-})
+// dotenv.config({
+//     path: './.env'
+// })
 
 
 const port = process.env.PORT || 4000
