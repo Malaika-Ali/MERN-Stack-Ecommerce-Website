252a9dba1f53a9b4b6b5d6339527b4b1c1a60bd8
Malaika Ali <114911365+Malaika-Ali@users.noreply.github.com>
2025-02-22 03:00:07 +0500

    set dispatch in googleAuthResponse

--- frontend/src/utils/googleAuthResponse.js
+++ frontend/src/utils/googleAuthResponse.js
@@ -1,20 +1,23 @@
 import { googleAuth } from '../pages/auth/googleApi'
 import { useNavigate } from 'react-router-dom'
+import { setUser } from '../redux/features/auth/authSlice'
+import { useDispatch } from 'react-redux'
 
-const navigate=useNavigate()
+const navigate = useNavigate()
+const dispatch = useDispatch();
 
-export const responseGoogle= async(authResult)=>{
-    try {
-      if(authResult['code']){
-        const result=await googleAuth(authResult['code'])
-        console.log(result)
-        const {email, name}=result.data.user
-        console.log('Result.data.user from login page', result?.data?.user)
-        navigate("/")
-      }
-      console.log(authResult)
-    } catch (error) {
-      console.log("Error while requesting Google code!",error)
+
+export const responseGoogle = async (authResult) => {
+  try {
+    if (authResult['code']) {
+      const result = await googleAuth(authResult['code'])
+      dispatch(setUser(result.data.user));
+      const { email, name } = result.data.user
+      console.log('Result.data.user from login page', result?.data?.user)
+      navigate("/")
     }
-      }
-    
+    console.log(authResult)
+  } catch (error) {
+    console.log("Error while requesting Google code!", error)
+  }
+}
