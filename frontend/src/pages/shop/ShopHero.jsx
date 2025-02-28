28dac40ec3fd11bbd7a6114b869d9565a75ceb8c
Malaika Ali <114911365+Malaika-Ali@users.noreply.github.com>
2025-03-01 00:57:05 +0500

    set z-index of shop hero lower than navbar for proper scrolling

--- frontend/src/pages/shop/ShopHero.jsx
+++ frontend/src/pages/shop/ShopHero.jsx
@@ -2,12 +2,12 @@ import bg4 from "../../assets/new.png"
 
 export default function ShopHero() {
   return (
-    <div className="relative min-h-[400px]  max-w-7xl mx-4 lg:mx-auto overflow-hidden bg-gray-100 rounded-3xl my-2 px-8 ">
+    <div className="relative min-h-[400px]  max-w-7xl mx-4 lg:mx-auto overflow-hidden bg-gray-100 rounded-3xl my-2 px-8 z-[5]">
       {/* Content Wrapper */}
       <div className="flex flex-col md:flex-row items-center justify-between h-full py-8 md:py-0">
         {/* Text Content */}
         <div className="relative z-10 w-full md:w-1/2 mb-8 md:mb-0">
-          <h2 className="text-2xl sm:text-3xl md:text-4xl font-bold leading-tight text-stone-800 mb-4">
+          <h2 className="text-2xl sm:text-3xl md:text-4xl font-bold leading-tight text-stone-800 mb-4 z-20">
             20% OFF ONLY TODAY AND GET SPECIAL GIFT!
           </h2>
           <p className="text-stone-600 text-sm sm:text-base">
