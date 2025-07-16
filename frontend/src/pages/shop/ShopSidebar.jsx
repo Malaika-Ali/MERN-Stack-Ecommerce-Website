223d06b80764f7fff7129b4de118ad7ec8456c1a
Malaika Ali <114911365+Malaika-Ali@users.noreply.github.com>
2025-07-17 01:20:59 +0500

    fix error of importing React on top of shop sidebar component

--- frontend/src/pages/shop/ShopSidebar.jsx
+++ frontend/src/pages/shop/ShopSidebar.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const ShopSidebar = ({ filters, filteredState, setFilteredState, clearFilters }) => {
   useEffect(() => console.log("sidebar rendered"))
