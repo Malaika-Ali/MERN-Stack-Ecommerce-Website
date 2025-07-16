30a0e160085b5767e63cac02d85c7be60b93da16
Malaika Ali <114911365+Malaika-Ali@users.noreply.github.com>
2025-07-17 01:24:56 +0500

    remove the useEffect hook from shop sidebar

--- frontend/src/pages/shop/ShopSidebar.jsx
+++ frontend/src/pages/shop/ShopSidebar.jsx
@@ -1,7 +1,6 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 
 const ShopSidebar = ({ filters, filteredState, setFilteredState, clearFilters }) => {
-  useEffect(() => console.log("sidebar rendered"))
 
   const [expandedSections, setExpandedSections] = useState({
     category: true,
